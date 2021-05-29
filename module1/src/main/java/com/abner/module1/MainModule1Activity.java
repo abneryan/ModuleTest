5a1982a0c8d9cce721debf5bab014afa9f8107fa
yanguoqing <yanguoqing@meishubao.com>
2021-05-29 18:59:14 +0800

    Initial commit

---
+++ module1/src/main/java/com/abner/module1/MainModule1Activity.java
@@ -0,0 +1,19 @@
+package com.abner.module1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * @Auther: admin
+ * @Date: 5/29/21 16:34
+ * @Description:
+ */
+public class MainModule1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
